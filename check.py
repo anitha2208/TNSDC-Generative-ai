405a97804ffd83ea10d343455a6fa3979704dfed
anitha2208 <146456734+anitha2208@users.noreply.github.com>
2025-09-12 11:11:36 +0530

    Delete check.py

--- check.py
+++
@@ -1,7 +0,0 @@
-from new import process_uploaded_csv
-result = process_uploaded_csv("uploads/BoTNeTIoT-L01-v2.csv")
-print("UUID:", result["uuid"])
-print("JSON Path:", result["json_path"])
-print("Numerical Columns:", result["numerical_columns"])
-print("Categorical Columns:", result["categorical_columns"])
-print("Datetime Columns:", result["datetime_columns"])
